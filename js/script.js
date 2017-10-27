e13debaca8432a94dfe530d60bc73cb37306e839
Amol <29180487+frextrite@users.noreply.github.com>
2017-10-27 19:54:53 +0530

    Fixed methods to use BigInteger library properly

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 // Algorithm hints taken from http://www.geeksforgeeks.org/primality-test-set-3-miller-rabin/
 
 function isPrime(num, k) {
-    if(bigInt(num).eq(2))
+    console.log('num in func '+num);
+    if(bigInt(num.toString()).eq(2))
         return true;
-    if(bigInt(num).mod(2).eq(0))
+    if(bigInt(num.toString()).mod(2).eq(0))
         return false;
 
-    var d = bigInt(num - 1), r = 0;
+    var d = bigInt(num.toString()).minus(1), r = 0;
     while(d.mod(2).eq(0)) {
         d = d.divide(2);
         r++;
     }
 
+    console.log('d'+d.toString());
+    console.log('r'+r);
     for(var i = 0; i < k; i++) {
         if(millerTest(num, d, r) == false) {
             return false;
@@ -22,26 +25,35 @@ function isPrime(num, k) {
 }
 
 function power(a, d, p) {
-    a = bigInt(a).mod(p);
-    if(bigInt(d).eq(0))
+    //console.log('in power d'+d.toString());
+    //console.log('in power a'+a.toString());
+    //console.log('in power p'+p.toString());
+    a = bigInt(a.toString()).mod(bigInt(p.toString()));
+    if(bigInt(d.toString()).eq(0))
         return 1;
     if(bigInt(bigInt(d).mod(2)).eq(1))
-        return bigInt(a.multiply(bigInt(power(a, d-1, p)).mod(p))).mod(p); //return (a * (power(a, d - 1, p) % p)) % p;
-    var n = bigInt(power(a, bigInt(d/2), bigInt(p))).mod(p);
+        return bigInt(bigInt(a.toString()).multiply(bigInt(power(a, d.minus(1), p)).mod(bigInt(p.toString())))).mod(bigInt(p.toString())); //return (a * (power(a, d - 1, p) % p)) % p;
+    var n = bigInt(power(a, d.divide(2), p)).mod(bigInt(p.toString()));
     return bigInt(n.multiply(n)).mod(p);
 }
 
 function millerTest(num, d, r) {
-    var a = bigInt(Math.floor(Math.random() * (num - 4) + 2));
+    console.log('num'+num);
+    //var a = bigInt(Math.floor(Math.random() * (num - 4) + 2));
+    //var a = bigInt(Math.floor(Math.random() * (num - 4))).add(2);
+    var a = bigInt.randBetween(2, num - 4).toString();
+    console.log('a'+a);
     var x = bigInt(power(a, d, num));
-    if(x.eq(1) || x.eq(num - 1))
+    console.log('x'+x.toString());
+    if(x.eq(1) || x.eq(bigInt(num.toString()).minus(1)))
         return true;
 
     while(r-1 >= 0) {
-        x = x.mod(num).multiply(x.mod(num)).mod(num);
+        x = x.mod(bigInt(num.toString())).multiply(x.mod(bigInt(num.toString()))).mod(bigInt(num.toString()));
+        console.log('x'+x.toString());
         if(x.eq(1))
             return false;
-        if(x.eq(num-1))
+        if(x.eq(bigInt(num.toString()).minus(1)))
             return true;
         r--;
     }
@@ -49,7 +61,8 @@ function millerTest(num, d, r) {
 }
 
 function checkPrime() {
-    var primeToCheck = parseInt(document.getElementById("user_input").value);
+    var primeToCheck = document.getElementById("user_input").value;
+    console.log(primeToCheck);
     var k = 4;
     if(primeToCheck == 1) {
         alert('1 is neither a prime nor composite');
